710b859b939d7303c2c8540bca5034884143768e
Yang Li <yanglited@gmail.com>
2023-07-08 15:16:22 -0400

    add plugin jamestthompson3/nvim-remote-containers

--- lua/josean/plugins-setup.lua
+++ lua/josean/plugins-setup.lua
@@ -104,6 +104,8 @@ return packer.startup(function(use)
   use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
   use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
 
+  use("jamestthompson3/nvim-remote-containers")
+
   -- git integration
   use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
