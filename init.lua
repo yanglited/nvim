3967b1920c8a48e3c44bd394753e3ec3162a41ed
Yang Li <yanglited@gmail.com>
2023-07-08 22:49:00 -0400

    added comments showing what was installed

--- init.lua
+++ init.lua
@@ -1,3 +1,21 @@
+-- installs:
+--11  sudo apt install git
+--31  sudo apt-get install neovim
+--75  sudo apt-get install ripgrep
+--88  sudo apt install build-essential
+--90  sudo apt-get install cmake
+--92  sudo apt-get install gettext
+--94  sudo make install
+--109  sudo apt install python3.10-venv
+--124  sudo apt install nodejs
+--128  sudo apt install npm
+--149  sudo apt-get install ca-certificates curl gnupg
+--150  sudo install -m 0755 -d /etc/apt/keyrings
+--156  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+--273  sudo apt install htop
+--274  sudo apt install fzf
+--278  history |grep install
+
 require("josean.plugins-setup")
 require("josean.core.options")
 require("josean.core.keymaps")
